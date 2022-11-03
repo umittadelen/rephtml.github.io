3f3b7514813c34a64562af2f2415759c9e2b98a6
umittadelen <72702832+umit-09@users.noreply.github.com>
2022-11-03 19:16:58 +0100

    Summary

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-function p1_to_p2() {
-    document.getElementById("div1").style.display="none";
-    document.getElementById("div2").style.display="block";
-}
-function p2_to_p1() {
+function to_p1() {
     document.getElementById("div1").style.display="block";
     document.getElementById("div2").style.display="none";
+    document.getElementById("div3").style.display="none";
+}
+function to_p2() {
+    document.getElementById("div1").style.display="none";
+    document.getElementById("div2").style.display="block";
+    document.getElementById("div3").style.display="none";
+}
+function to_p3() {
+    document.getElementById("div1").style.display="none";
+    document.getElementById("div2").style.display="none";
+    document.getElementById("div3").style.display="block";
 }
