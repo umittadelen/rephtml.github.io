f4a6c1868f9ce4ed84815c97910a2bfd1e83e373
umittadelen <72702832+umit-09@users.noreply.github.com>
2022-11-03 17:54:26 +0100

    Summary

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function p1_to_p2() {
     document.getElementById("div1").style.display="none";
     document.getElementById("div2").style.display="block";
 }
-    function p2_to_p1() {
+function p2_to_p1() {
     document.getElementById("div1").style.display="block";
     document.getElementById("div2").style.display="none";
 }
