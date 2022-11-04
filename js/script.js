68322d8861fde1def162af3ed283f1d8297c390d
umittadelen <72702832+umit-09@users.noreply.github.com>
2022-11-04 17:18:45 +0100

    Summary

--- js/script.js
+++ js/script.js
@@ -2,14 +2,23 @@ function to_p1() {
     document.getElementById("div1").style.display="block";
     document.getElementById("div2").style.display="none";
     document.getElementById("div3").style.display="none";
+    document.getElementById("div4").style.display="none";
 }
 function to_p2() {
     document.getElementById("div1").style.display="none";
     document.getElementById("div2").style.display="block";
     document.getElementById("div3").style.display="none";
+    document.getElementById("div4").style.display="none";
 }
 function to_p3() {
     document.getElementById("div1").style.display="none";
     document.getElementById("div2").style.display="none";
     document.getElementById("div3").style.display="block";
+    document.getElementById("div4").style.display="none";
+}
+function to_p4() {
+    document.getElementById("div1").style.display="none";
+    document.getElementById("div2").style.display="none";
+    document.getElementById("div3").style.display="none";
+    document.getElementById("div4").style.display="block";
 }
