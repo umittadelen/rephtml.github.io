271033dad4be40b4e151c2210a349005cadfa43f
umittadelen <72702832+umit-09@users.noreply.github.com>
2022-11-07 18:18:37 +0100

    Summary

---
+++ js/page2.js
@@ -0,0 +1,19 @@
+const wrapper = document.querySelector('.infinite')
+const neck = document.querySelector('.neck')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.intersectionRatio > 0){
+            injectneck(entry)
+        }
+    });
+}, {});
+
+function injectneck(entry){
+    observer.unobserve(entry.target)
+    const clonedneck = neck.cloneNode(true)
+    wrapper.appendChild(clonedneck)
+    observer.observe(clonedneck)
+}
+
+observer.observe(neck)
