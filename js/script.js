0a70a2e57462ecc0caff2569756c6b8c7d13b444
umittadelen <72702832+umit-09@users.noreply.github.com>
2022-11-11 17:17:38 +0100

    Summary

--- js/script.js
+++ js/script.js
@@ -6,22 +6,33 @@ function to_p1() {
     document.getElementById("div2").style.display="none";
     document.getElementById("div3").style.display="none";
     document.getElementById("div4").style.display="none";
+    document.getElementById("div5").style.display="none";
 }
 function to_p2() {
     document.getElementById("div1").style.display="none";
     document.getElementById("div2").style.display="block";
     document.getElementById("div3").style.display="none";
     document.getElementById("div4").style.display="none";
+    document.getElementById("div5").style.display="none";
 }
 function to_p3() {
     document.getElementById("div1").style.display="none";
     document.getElementById("div2").style.display="none";
     document.getElementById("div3").style.display="block";
     document.getElementById("div4").style.display="none";
+    document.getElementById("div5").style.display="none";
 }
 function to_p4() {
     document.getElementById("div1").style.display="none";
     document.getElementById("div2").style.display="none";
     document.getElementById("div3").style.display="none";
     document.getElementById("div4").style.display="block";
+    document.getElementById("div5").style.display="none";
+}
+function to_p5() {
+    document.getElementById("div1").style.display="none";
+    document.getElementById("div2").style.display="none";
+    document.getElementById("div3").style.display="none";
+    document.getElementById("div4").style.display="none";
+    document.getElementById("div5").style.display="block";
 }
